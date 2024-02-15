b5d5ff2c0e54f679bbfa09a919921d80107f0e42
Abir Hossen Ornob <abirhosain090@gmail.com>
2024-02-15 19:27:36 +0600

    Keyup Event handler

--- js/alpha.js
+++ js/alpha.js
@@ -1,3 +1,13 @@
+function handleKeyboard(event) {
+  const playerPressed = event.key;
+  const randomCharacter = document.getElementById("Alphabet").innerText;
+  if (playerPressed == randomCharacter) {
+    console.log("You Got Some Point");
+  } else {
+    console.log("You have Lost A Life");
+  }
+}
+document.addEventListener("keyup", handleKeyboard);
 function continueGame() {
   const randomCharacter = getRandomAlphabet();
   const alphabet = (document.getElementById("Alphabet").innerHTML =
