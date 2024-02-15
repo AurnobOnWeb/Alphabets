d9a63030748acae0557f0e6796ac2af43a1614eb
Abir Hossen Ornob <abirhosain090@gmail.com>
2024-02-15 19:08:16 +0600

    Game Area Section

--- js/alpha.js
+++ js/alpha.js
@@ -1,4 +1,12 @@
+function continueGame() {
+  const randomCharacter = getRandomAlphabet();
+  const alphabet = (document.getElementById("Alphabet").innerHTML =
+    randomCharacter);
+  addBgClass(randomCharacter);
+}
+
 function playNow() {
   hideElement("home");
   showElement("gameArea");
+  continueGame();
 }
--- js/utility.js
+++ js/utility.js
@@ -7,3 +7,18 @@ function showElement(elementId) {
   const element = document.getElementById(elementId);
   element.classList.remove("hidden");
 }
+function addBgClass(elementId) {
+  const element = document.getElementById(elementId);
+  element.classList.add("bg-orange-700");
+}
+
+function getRandomAlphabet() {
+  const alphabetString = "abcdefghijklmnopqrstuvxwyz";
+  const alphabetArray = alphabetString.split("");
+
+  const randomNumber = Math.random() * 25;
+  const randomIndex = Math.round(randomNumber);
+
+  const randomCharacter = alphabetArray[randomIndex];
+  return randomCharacter;
+}
